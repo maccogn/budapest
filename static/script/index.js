2c1469ab96d22952e54319991a70e4920df25946
maccogn <35314560+maccogn@users.noreply.github.com>
2018-08-25 08:51:10 +0200

    Add files via upload

--- static/script/index.js
+++ static/script/index.js
@@ -44,5 +44,60 @@ sliderPage = {
     }
 };
 
+
+let mover;
+
+mover = {
+        showBoards: () => {
+        let sideBoard = document.getElementById("sideBoard");
+        let allBoard =`<div class="container1" id="right-section">
+                  <div class="d4">
+                    <img src="static/img/asset2%20(1).png" class="image2">
+                      <div class="middle2">
+                        <div class="text2">Welcome to Budapest!</div>
+                      </div>
+                  </div>
+                  <div class="d3">
+                    <img src="static/img/asset1%20(1).png" class="image1">
+                      <div class="middle1">
+                        <div class="text1">Welcome to Budapest!</div>
+                      </div>
+                  </div>
+                </div>
+              </div>`;
+        mover.appendToElement(sideBoard, allBoard);
+    },
+    appendToElement: function (elementToExtend, textToAppend, prepend = false) {
+        // function to append new DOM elements (represented by a string) to an existing DOM element
+        let fakeDiv = document.createElement('div');
+        fakeDiv.innerHTML = textToAppend.trim();
+
+        for (childNode of fakeDiv.childNodes) {
+            if (prepend) {
+                elementToExtend.prependChild(childNode);
+            } else {
+                elementToExtend.appendChild(childNode);
+            }
+        }
+        return elementToExtend.lastChild;
+    },
+    deleteBoards: () => {
+            let rightSection = document.getElementById('right-section');
+            rightSection.parentNode.removeChild(rightSection);
+    },
+    deleteCollapseExample: () => {
+        let rightSection = document.getElementById('collapseExample');
+        rightSection.parentNode.removeChild(rightSection);
+    },
+    moverEventListeners: () => {
+        let beginBtn = document.getElementById('moverBtn');
+        beginBtn.addEventListener("click", mover.deleteBoards);
+        let secondScreen = document.getElementById('collapseExample');
+        secondScreen.addEventListener('click', mover.showBoards);
+    }
+};
+
+
 colorBoxChanger.eventListeners();
 sliderPage.sliderEvent();
+mover.moverEventListeners();
